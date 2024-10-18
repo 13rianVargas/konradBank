0108ec9dac2da0f6ac5143655c9f086740146255
Dywrench <jv88611@gmail.com>
2024-10-18 17:13:39 -0500

    FIXED-> cambio de atributos y metodos clase ahorros
    se modifico toda la clase ahorros para implementar los nuevos atributos y metodos.

--- src/co/edu/konradlorenz/model/CuentaAhorro.java
+++ src/co/edu/konradlorenz/model/CuentaAhorro.java
@@ -2,15 +2,44 @@ package co.edu.konradlorenz.model;
 
 public class CuentaAhorro extends Cuenta {
     private double tasaInteres;
+    private int limiteRetiros;
+    private double saldoMinimo;
+    private int retirosRealizados;  
 
-    
-    public CuentaAhorro() {
-        super();
+   
+    public CuentaAhorro(String numeroCuenta, double saldo, String titular, double tasaInteres, int limiteRetiros, double saldoMinimo) {
+        super(numeroCuenta, saldo, titular);
+        this.tasaInteres = tasaInteres;
+        this.limiteRetiros = limiteRetiros;
+        this.saldoMinimo = saldoMinimo;
+        this.retirosRealizados = 0;  
     }
 
-    public CuentaAhorro(String numeroCuenta, double saldo, String titular, double tasaInteres) {
-        super(numeroCuenta, saldo, titular); 
-        this.tasaInteres = tasaInteres;
+   
+    public void realizarRetiro(double monto) {
+        if (retirosRealizados >= limiteRetiros) {
+            System.out.println("Has alcanzado el límite de retiros mensuales.");
+        } else if (getSaldo() - monto < saldoMinimo) {
+            System.out.println("No puedes retirar, el saldo caería por debajo del mínimo requerido.");
+        } else {
+            setSaldo(getSaldo() - monto);
+            retirosRealizados++;
+            System.out.println("Retiro realizado. Nuevo saldo: " + getSaldo());
+        }
+    }
+
+    public void resetearRetirosMensuales() {
+        retirosRealizados = 0;
+    }
+
+    
+    public double calcularInteres() {
+        return getSaldo() * tasaInteres;
+    }
+
+    public void acreditarInteres() {
+        double interes = calcularInteres();
+        setSaldo(getSaldo() + interes);
     }
 
     public double getTasaInteres() {
@@ -21,18 +50,8 @@ public class CuentaAhorro extends Cuenta {
         this.tasaInteres = tasaInteres;
     }
 
-    public double calcularInteres() {
-        return getSaldo() * tasaInteres; 
-    }
-
-    
-    public void acreditarInteres() {
-        double interes = calcularInteres();
-        setSaldo(getSaldo() + interes); 
-    }
-
     @Override
     public String toString() {
-        return super.toString() + ", Tasa de Interés: " + tasaInteres;
+        return super.toString() + ", Tasa de Interés: " + tasaInteres + ", Límite de Retiros: " + limiteRetiros + ", Saldo Mínimo: " + saldoMinimo;
     }
 }
