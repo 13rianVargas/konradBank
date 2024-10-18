a9ea85d600dd2619360cfbfc6e575e0965ba4b24
Camilo Prieto <prietocamilo21@gmail.com>
2024-10-18 18:40:41 -0500

    READY - ModelCamilo
    Se concretó la implementación de código en cada clase creada en esta rama y se considera que ya esta lista para una revisión y aprobación para unirla al main.

--- src/co/edu/konradlorenz/model/Cajero.java
+++ src/co/edu/konradlorenz/model/Cajero.java
@@ -1,5 +1,34 @@
 package co.edu.konradlorenz.model;
 
+import java.time.LocalDate;
+
 public class Cajero {
+
+	private LocalDate fecha;
+
+	public Cajero(LocalDate fecha) {
+		super();
+		this.fecha = fecha;
+	}
+
+	public Cajero() {
+	}
+
+	public LocalDate getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(LocalDate fecha) {
+		this.fecha = fecha;
+	}
+
+	public boolean verificarTarjeta(Tarjeta tarjeta) {
+		return tarjeta.esTarjetaValida();
+	}
+
+	@Override
+	public String toString() {
+		return "Cajero [fecha=" + fecha + "]";
+	}
 
 }
--- src/co/edu/konradlorenz/model/Cliente.java
+++ src/co/edu/konradlorenz/model/Cliente.java
@@ -2,4 +2,48 @@ package co.edu.konradlorenz.model;
 
 public class Cliente {
 
+	private String nombre;
+	private long numeroCuenta;
+	private long Cedula;
+
+	public Cliente(String nombre, long numeroCuenta, long cedula) {
+		super();
+		this.nombre = nombre;
+		this.numeroCuenta = numeroCuenta;
+		Cedula = cedula;
+	}
+
+	public Cliente() {
+
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public long getNumeroCuenta() {
+		return numeroCuenta;
+	}
+
+	public void setNumeroCuenta(long numeroCuenta) {
+		this.numeroCuenta = numeroCuenta;
+	}
+
+	public long getCedula() {
+		return Cedula;
+	}
+
+	public void setCedula(long cedula) {
+		Cedula = cedula;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [nombre=" + nombre + ", numeroCuenta=" + numeroCuenta + ", Cedula=" + Cedula + "]";
+	}
+	
 }
