29001a9c96a9b41966d8db06136af1e71d3024c9
Dywrench <jv88611@gmail.com>
2024-10-18 19:46:26 -0500

    FIX----> eliminacion metodos Clase Corriente
    se eliminaron los metodos innecesarios de la clase corriente incluyendo los que implementaban sout.

--- src/co/edu/konradlorenz/model/Corriente.java
+++ src/co/edu/konradlorenz/model/Corriente.java
@@ -1,13 +1,13 @@
 package co.edu.konradlorenz.model;
 
-public class CuentaCorriente extends Cuenta {
+public class Corriente extends Cuenta {
     private double tasaSobregiro;
     private double limiteSobregiro;
     private double comisionMantenimiento;
     private double sobregiroActual;
 
    
-    public CuentaCorriente(String numeroCuenta, double saldo, String titular, double tasaSobregiro, double limiteSobregiro, double comisionMantenimiento) {
+    public Corriente(String numeroCuenta, double saldo, String titular, double tasaSobregiro, double limiteSobregiro, double comisionMantenimiento) {
         super(numeroCuenta, saldo, titular);
         this.tasaSobregiro = tasaSobregiro;
         this.limiteSobregiro = limiteSobregiro;
@@ -15,20 +15,7 @@ public class CuentaCorriente extends Cuenta {
         this.sobregiroActual = 0;  
     }
 
-    public void aplicarSobregiro(double monto) {
-        if (sobregiroActual + monto > limiteSobregiro) {
-            System.out.println("El monto excede el límite de sobregiro.");
-        } else {
-            sobregiroActual += monto;
-            setSaldo(getSaldo() - monto);
-            System.out.println("Sobregiro aplicado. Sobregiro actual: " + sobregiroActual);
-        }
-    }
-
-    public void cobrarComision() {
-        setSaldo(getSaldo() - comisionMantenimiento);
-        System.out.println("Comisión de mantenimiento aplicada. Nuevo saldo: " + getSaldo());
-    }
+    
 
     public double getTasaSobregiro() {
         return tasaSobregiro;
@@ -53,8 +40,17 @@ public class CuentaCorriente extends Cuenta {
     public void setComisionMantenimiento(double comisionMantenimiento) {
         this.comisionMantenimiento = comisionMantenimiento;
     }
+    
 
-    @Override
+    public double getSobregiroActual() {
+		return sobregiroActual;
+	}
+
+	public void setSobregiroActual(double sobregiroActual) {
+		this.sobregiroActual = sobregiroActual;
+	}
+
+	@Override
     public String toString() {
         return super.toString() + ", Tasa de Sobregiro: " + tasaSobregiro + ", Límite de Sobregiro: " + limiteSobregiro + ", Comisión de Mantenimiento: " + comisionMantenimiento;
     }
