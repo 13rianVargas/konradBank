b30e051f5b08344e126f2ff9b84032120f394bab
Dywrench <jv88611@gmail.com>
2024-10-21 15:20:03 -0500

    New------> Base del controller
    Se creo una base del control asumiendo cosas que se harán en el GUI pero representados como "vista" eso debe ser luego remplazado para obtener los datos del cliente (pin) y las opciones (retirar/depositar)
    Por ultimo, aclaro que todo esta comentareado para que no salgan errores, los cuales van a ser solucionados al momento que hagamos el GUI.

--- src/co/edu/konradlorenz/controller/Controlador.java
+++ src/co/edu/konradlorenz/controller/Controlador.java
@@ -7,5 +7,57 @@ public class Controlador {
 
     public void run() {
         
+    	// Solicitar datos del cliente, 
+    	//aca no se como se hace con el GUI pero supongo que solo sera cambiarlo xd
+    	 /*
+        String nombre = "pepito";
+        long numeroCuenta = 423;
+        long numeroDocumento = 976123;
+        String numeroTarjeta ="89524633" ;
+        String pin = "123";
+        
+       
+        Cliente persona = new Cliente(nombre, numeroCuenta, numeroDocumento, numeroTarjeta, pin);
+
+        // Verificar si la tarjeta es válida
+        
+        /*
+        if (cliente.esTarjetaValida()) {
+            vista.mostrarMensaje("Tarjeta válida, Que operacion desea hacer.");
+        } else {
+            vista.mostrarMensaje("Tarjeta inválida.");
+            return;
+        }
+    	
+        boolean salir = false;
+        while (!salir) {
+            int opcion = vista.mostrarMenu(); //aca supongamos que es lo del gui pues para retirar o hacer deposito
+
+            switch (opcion) {
+                case 1: // Depositar dinero
+                    double montoDeposito = vista.solicitarMonto();
+                    cuenta.depositar(montoDeposito);
+                    vista.mostrarMensaje("Depósito realizado. Saldo actual: " + Cuenta.getSaldo());
+                    break;
+                case 2: // Retirar dinero
+                    double montoRetiro = vista.solicitarMonto();
+                    Cuenta.retirar(montoRetiro);
+                    vista.mostrarMensaje("Retiro realizado. Saldo actual: " + Cuenta.getSaldo());
+                    break;
+               
+                case 3: // Salir
+                    salir = true;
+                  //lo dejare aca como un mensaje de salida y el resumen de la transacion obvio toca en el gui xd
+                    vista.mostrarMensaje("Gracias por preferirnos -KonradBank-."); 
+                    vista.mostrarCuenta(Cuenta.toString());
+                    break;
+            }
+        }
     }
 }
+    	
+
+    	
+    }*/ 
+    	}  
+ }
