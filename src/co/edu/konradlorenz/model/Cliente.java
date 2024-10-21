53fd611d3260bcd26714b5d89cb521e888eecda3
Camilo Prieto <prietocamilo21@gmail.com>
2024-10-20 19:24:59 -0500

    FIXED - ModelCamilo
    Se corrigieron las observaciones dadas en el Pull Request sobre atributos, getters, setters y contructores.

--- src/co/edu/konradlorenz/model/Cliente.java
+++ src/co/edu/konradlorenz/model/Cliente.java
@@ -1,16 +1,20 @@
 package co.edu.konradlorenz.model;
 
-public class Cliente {
+public class Cliente implements Tarjeta {
 
 	private String nombre;
 	private long numeroCuenta;
-	private long Cedula;
+	private long numeroDocumento;
+	private String numeroTarjeta;
+	private String pin;
 
-	public Cliente(String nombre, long numeroCuenta, long cedula) {
+	public Cliente(String nombre, long numeroCuenta, long numeroDocumento, String numeroTarjeta, String pin) {
 		super();
 		this.nombre = nombre;
 		this.numeroCuenta = numeroCuenta;
-		Cedula = cedula;
+		this.numeroDocumento = numeroDocumento;
+		this.numeroTarjeta = numeroTarjeta;
+		this.pin = pin;
 	}
 
 	public Cliente() {
@@ -33,17 +37,59 @@ public class Cliente {
 		this.numeroCuenta = numeroCuenta;
 	}
 
-	public long getCedula() {
-		return Cedula;
+	public long getNumeroDocumento() {
+		return numeroDocumento;
 	}
 
-	public void setCedula(long cedula) {
-		Cedula = cedula;
+	public void setNumeroDocumento(long numeroDocumento) {
+		this.numeroDocumento = numeroDocumento;
+	}
+
+	public String getNumeroTarjeta() {
+		return numeroTarjeta;
+	}
+
+	public void setNumeroTarjeta(String numeroTarjeta) {
+		this.numeroTarjeta = numeroTarjeta;
+	}
+
+	public String getPin() {
+		return pin;
+	}
+
+	public void setPin(String pin) {
+		this.pin = pin;
 	}
 
 	@Override
 	public String toString() {
-		return "Cliente [nombre=" + nombre + ", numeroCuenta=" + numeroCuenta + ", Cedula=" + Cedula + "]";
+		return "Cliente [nombre=" + nombre + ", numeroCuenta=" + numeroCuenta + ", numeroDocumento=" + numeroDocumento
+				+ ", numeroTarjeta=" + numeroTarjeta + ", pin=" + pin + "]";
 	}
-	
+
+	@Override
+	public boolean esTarjetaValida() {
+		int suma = 0;
+		boolean alternar = false;
+
+		for (int i = numeroTarjeta.length() - 1; i >= 0; i--) {
+			int digito = Character.getNumericValue(numeroTarjeta.charAt(i));
+			if (alternar) {
+				digito *= 2;
+				if (digito > 9) {
+					digito -= 9;
+				}
+			}
+			suma += digito;
+			alternar = !alternar;
+		}
+		return suma % 10 == 0;
+	}
+
+	@Override
+	public boolean verificarPIN(String pin) {
+
+		return this.pin.equals(pin);
+	}
+
 }
--- src/co/edu/konradlorenz/model/Tarjeta.java
+++ src/co/edu/konradlorenz/model/Tarjeta.java
@@ -1,8 +1,8 @@
 package co.edu.konradlorenz.model;
 
 public interface Tarjeta {
-	
-	String getNumeroTarjeta();
+
 	boolean verificarPIN(String pin);
+
 	boolean esTarjetaValida();
 }
